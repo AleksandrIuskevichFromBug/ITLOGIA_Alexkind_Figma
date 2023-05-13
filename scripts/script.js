06c57a5b2c794bd81a8fa83d6419376e2a32fc8d
AleksandrIuskevichFromBug <107239203+AleksandrIuskevichFromBug@users.noreply.github.com>
2023-05-13 15:41:26 +0000

    onclick

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,11 @@
-
+/*document.getElementsByClassName("main-title")[0].style.color = "red";
+document.getElementsByClassName("main-title")[0].style.font-size = 56px;*/
+document.getElementById("main-action-button").onclick = function(){
+    document.getElementById("products").scrollIntoView({behavior:"smooth"});
+}
+let links = document.querySelectorAll(".menu-item > a");
+for (let i = 0; i < links.length; i++){
+    links[i].onclick = function () {
+      document.getElementById(links[i].getAttribute("data-link")).scrollIntoView({behavior:"smooth"});  
+    }
+}
