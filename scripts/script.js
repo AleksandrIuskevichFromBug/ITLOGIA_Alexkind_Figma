cd679e80297c700ab36866602b55b1999c752248
AleksandrIuskevichFromBug <107239203+AleksandrIuskevichFromBug@users.noreply.github.com>
2023-05-13 16:47:35 +0000

    js_bye

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,37 @@
 /*document.getElementsByClassName("main-title")[0].style.color = "red";
 document.getElementsByClassName("main-title")[0].style.font-size = 56px;*/
-document.getElementById("main-action-button").onclick = function(){
-    document.getElementById("products").scrollIntoView({behavior:"smooth"});
+document.getElementById("main-action-button").onclick = function() {
+    document.getElementById("products").scrollIntoView({ behavior: "smooth" });
 }
 let links = document.querySelectorAll(".menu-item > a");
-for (let i = 0; i < links.length; i++){
-    links[i].onclick = function () {
-      document.getElementById(links[i].getAttribute("data-link")).scrollIntoView({behavior:"smooth"});  
+for (let i = 0; i < links.length; i++) {
+    links[i].onclick = function() {
+        document.getElementById(links[i].getAttribute("data-link")).scrollIntoView({ behavior: "smooth" });
+    }
+}
+let buttons = document.getElementsByClassName("product-button");
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].onclick = function() {
+        document.getElementById("order").scrollIntoView({ behavior: "smooth" });
+    }
+}
+let burger = document.getElementById("burger");
+let name = document.getElementById("name");
+let phone = document.getElementById("phone");
+document.getElementById("order-action").onclick = function(){
+    let hasError = false;
+    [burger, name, phone].forEach( item => {
+        if(!item.value) {
+         item.parentElement.style.background = "red";
+            hasError = true;
+        } else {
+          item.parentElement.style.background = "";  
+        }
+    });
+    if (!hasError){
+        [burger, name, phone].forEach( item => {
+            item.value = "";
+        });
+        alert("Спасибо за закакз! Мы скоро свяжемся с вами!");
     }
 }
