4ea2e05a2d18ad5c142360540de567a18b62fdf3
AleksandrIuskevichFromBug <107239203+AleksandrIuskevichFromBug@users.noreply.github.com>
2023-05-13 18:38:16 +0000

    script_end

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,3 @@
-/*document.getElementsByClassName("main-title")[0].style.color = "red";
-document.getElementsByClassName("main-title")[0].style.font-size = 56px;*/
 document.getElementById("main-action-button").onclick = function() {
     document.getElementById("products").scrollIntoView({ behavior: "smooth" });
 }
@@ -38,31 +36,31 @@ document.getElementById("order-action").onclick = function() {
 
 let prices = document.getElementsByClassName("products-item-price");
 
-document.getElementById("change-currency").onclick = function(e){
- let currentCurrency = e.target.innerText;  
- let newCurrency = "$"; 
- let coefficient = 1;   
+document.getElementById("change-currency").onclick = function(e) {
+    let currentCurrency = e.target.innerText;
+    let newCurrency = "$";
+    let coefficient = 1;
     if (currentCurrency === "$") {
         newCurrency = "₽";
-        
-        coefficient = 80;
-        
-    } else if (currentCurrency === "₽"){
-        newCurrency = "BYN";
-        
-        coefficient = 3;
-    }else if (currentCurrency === 'BYN') {
-    newCurrency = '€';
-    coefficient = 0.9;
-} else if (currentCurrency === '€') {
-    newCurrency = '¥';
-    coefficient = 6.9;
-}
 
-   e.target.innerText = newCurrency; 
+        coefficient = 80;
+
+    } else if (currentCurrency === "₽") {
+        newCurrency = "BYN";
+
+        coefficient = 3;
+    } else if (currentCurrency === 'BYN') {
+        newCurrency = '€';
+        coefficient = 0.9;
+    } else if (currentCurrency === '€') {
+        newCurrency = '¥';
+        coefficient = 6.9;
+    }
+
+    e.target.innerText = newCurrency;
 
     for (let i = 0; i < prices.length; i++) {
         prices[i].innerText = +(prices[i].getAttribute("data-base-price") * coefficient).toFixed(1) + " " + newCurrency;
     }
-        
+
 }
